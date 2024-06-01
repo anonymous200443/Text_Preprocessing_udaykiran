a74b57b17a4d12c47542a5b8b0d90e35d0818a25
UdayKiran <udaykiran7615124@gmail.com>
2024-06-01 17:50:55 +0530

    python packages for https://bit.ly/intro_nlp

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Readme.md
+
+# Processing Text Package
+
+course Link:https://bit.ly/intro_nlp
---
+++ Text_Preprocessing_udaykiran/__init__.py
@@ -0,0 +1,22 @@
+# __init__.py
+
+from .Utils import (
+    load_data,
+    char_counts,
+    word_counts,
+    stop_words_length,
+    numeric_count,
+    upper_case_count,
+    cont_to_exp,
+    remove_punctuation,
+    remove_extra_spaces,
+    remove_html,
+    remove_accented_chars,
+    lemmatize_text,
+    tokenize_text,
+    get_noun_chunks,
+    detect_language,
+    correct_spelling,
+    create_wordcloud,
+    get_sentiment
+)
