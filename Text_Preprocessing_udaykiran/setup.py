e4c69483812b6c245d4eb81ddc64fd47c9a0e452
udaykiran <143863214+anonymous200443@users.noreply.github.com>
2024-06-04 14:44:15 +0530

    Update setup.py

--- Text_Preprocessing_udaykiran/setup.py
+++ Text_Preprocessing_udaykiran/setup.py
@@ -1,27 +1,21 @@
-# setup.py
 import setuptools
 
-with open('Read.md','r') as file:
-	long_description=file.read()
-
-
-
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
 
 setuptools.setup(
-	name='Text_Preprocessing_udaykiran',
-	version='0.0.1',
-	author='Uday kiran',
-	author_email='Udaykiran7615124@gmail.com',
-	description='This is text_processing package',
-	long_description=long_description,
-	long_description_content_type=text/markdown,
-	packages=setuptools.find_packages(),
-	classifiers=[
-	'programming Language:: Python:: 3',
-	'License:: OSI Aproved :: MIT License',
-	'Operating System::OS Independent'],
-	python_requires='>=3.5'
-
-
-
-	)
+    name='Text_Preprocessing_udaykiran',
+    version='0.0.1',
+    author='Uday Kiran',
+    author_email='Udaykiran7615124@gmail.com',
+    description='This is a text_processing package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.5'
+)
